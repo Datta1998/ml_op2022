53fe90a5c0ca7a001f8b4a205f70eb64ba9b84ac
puplu <dattatreyoroy@gmail.com>
2022-11-14 09:52:53 +0530

    Updated

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask import request
 from joblib import load
 
 app = Flask(__name__)
-model_path = "svm_gamma=0.001_C=0.5.joblib"
+model_path = "svm_gamma=0.001_C=0.7.joblib"
 model = load(model_path)
 
 @app.route("/")
@@ -11,23 +11,14 @@ def hello_world():
     return "<!-- hello --> <b> Hello, World!</b>"
 
 
-# get x and y somehow    
-#     - query parameter
-#     - get call / methods
-#     - post call / methods ** 
-
-@app.route("/sum", methods=['POST'])
-def sum():
-    x = request.json['x']
-    y = request.json['y']
-    z = x + y 
-    return {'sum':z}
-
-
-
 @app.route("/predict", methods=['POST'])
 def predict_digit():
-    image = request.json['image']
+    digit_image1 = request.json['image']
+    digit_image2 = request.json['image']
     print("done loading")
-    predicted = model.predict([image])
-    return {"y_predicted":int(predicted[0])}
+    predicted_image1 = model.predict([digit_image1])
+    predicted_image2 = model.predict([digit_image2])
+    if predicted_image1==predicted_image2:
+        return "Same Class Images are found here"
+    else:
+        return "Same Class Images are not found here"    
