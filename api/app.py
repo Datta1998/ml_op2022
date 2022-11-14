b362b11c54dfcaef8b166d94db0581ccca874508
puplu <dattatreyoroy@gmail.com>
2022-11-14 11:55:24 +0530

    changes

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask import request
 from joblib import load
 
 app = Flask(__name__)
-model_path = "svm_gamma=0.001_C=0.5.joblib"
+model_path = "mod.joblib"
 model = load(model_path)
 
 @app.route("/")
