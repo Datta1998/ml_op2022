a9519f7a85ab5dd092538b9453753bc12723f342
puplu <dattatreyoroy@gmail.com>
2022-11-14 08:50:17 +0530

    Updated

--- api/app.py
+++ api/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import request
-from flask import jsonify
+from joblib import load
 
 app = Flask(__name__)
+model_path = "svm_gamma=0.001_C=0.5.joblib"
+model = load(model_path)
 
 @app.route("/")
 def hello_world():
     return "<!-- hello --> <b> Hello, World!</b>"
+
 
 # get x and y somehow    
 #     - query parameter
@@ -15,13 +18,16 @@ def hello_world():
 
 @app.route("/sum", methods=['POST'])
 def sum():
-    print(request.json)
     x = request.json['x']
     y = request.json['y']
     z = x + y 
-    return jsonify({'sum':z})  
-'''
-if __name__ == '__main__':
-    app.run(debug=True)
-'''
+    return {'sum':z}
 
+
+
+@app.route("/predict", methods=['POST'])
+def predict_digit():
+    image = request.json['image']
+    print("done loading")
+    predicted = model.predict([image])
+    return {"y_predicted":int(predicted[0])}
